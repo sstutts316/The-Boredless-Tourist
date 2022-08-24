a751967b209e235374d215fb29a259647a2187a3
Steven Stutts <sstutts316@gmail.com>
2022-08-24 14:32:45 -0500

    Added test object to test index functionality

---
+++ script.py
@@ -0,0 +1,7 @@
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+print(get_destination_index("Los Angeles, USA"))
